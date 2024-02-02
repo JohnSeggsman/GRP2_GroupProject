48807250a8486a1f1a7b9fbe26eb5426681ced05
Awxccc <sancturez@gmail.com>
2024-02-02 22:57:06 +0800

    First Update
    yes

---
+++ Assets/Scripts/BombScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    float bulletSpeed = 15f;
+    Rigidbody2D rb;
+    Animator animator;
+    PowerBarScript powerbarscript;
+    private void Awake()
+    {
+        powerbarscript = GameObject.Find("PowerBarCanvas").GetComponent<PowerBarScript>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 1;
+        SetStraightVelocity();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetStraightVelocity()
+    {
+        rb.velocity = transform.right * powerbarscript.currentPowerBarValue;
+    }
+    private void FixedUpdate()
+    {
+        if(rb.velocity.magnitude <= 0.3)
+        {
+            animator.GetComponent<Animator>().enabled = true;
+        }
+    }
+}
---
+++ Assets/SmoothCameraScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraScript : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    public float smoothTime;
+    private Vector3 velocity = Vector3.zero;
+    private Transform target;
+    public Throwable throwable;
+    private void Awake()
+    {
+        target = GameObject.Find("BOMB").transform;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(throwable.toggleOnce == true)
+        {
+            target = throwable.bombInst.transform;
+            Vector3 targetPosition = target.transform.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+        
+    }
+}
